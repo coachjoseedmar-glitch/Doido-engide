cd1d0bd4c5bb38dbd0d059780c3c14f489a469cb
valmarosaalves-cmyk <valmarosaalves@gmail.com>
2026-04-26 17:43:26 -0300

    Update WarningState.hx

--- source/states/WarningState.hx
+++ source/states/WarningState.hx
@@ -12,40 +12,51 @@ class WarningState extends MusicBeatState
 	var btnSim:FlxText;
 	var btnNao:FlxText;
 	
-	// Variável para saber qual botão está selecionado (Sim ou Não)
 	var selecionouSim:Bool = true; 
 
 	override public function create():Void 
 	{
 		super.create();
 
-		// 1. Carregar a imagem de fundo
-		avisoImg = new FlxSprite().loadGraphic(Paths.image('1000523347')); // Nome do seu PNG
+		// Fundo preto de segurança para garantir que nada apareça atrás
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		// 1. Carregar a imagem
+		// DICA: Certifique-se que o arquivo 1000523347.png está em: assets/images/
+		avisoImg = new FlxSprite();
+		avisoImg.loadGraphic(Paths.image('1000523347')); 
 		
-		// AJUSTE DE TAMANHO: Aumente os números abaixo se o PNG estiver pequeno
-		avisoImg.scale.set(1.2, 1.2); 
-		avisoImg.updateHitbox(); // Garante que o Haxe entenda o novo tamanho para centralizar
+		avisoImg.antialiasing = true; // Deixa a imagem mais bonita
+		avisoImg.scrollFactor.set();
+		avisoImg.updateHitbox();
 		avisoImg.screenCenter();
 		add(avisoImg);
 
+		// Se a imagem ainda estiver pequena demais, aumente aqui:
+		avisoImg.scale.set(1, 1); 
+		avisoImg.updateHitbox();
+		avisoImg.screenCenter();
+
 		// 2. Texto de Aviso
-		// O número 450 define a largura máxima. Se o texto bater ali, ele pula linha.
 		var mensagem:String = "Cuidado! Este jogo tem partes que podem prejudicar quem tem epilepsia. Deseja desabilitar as luzes piscantes?";
 		
-		popUpTxt = new FlxText(0, 0, 450, mensagem); 
-		popUpTxt.setFormat(Main.gFont, 24, FlxColor.WHITE, CENTER);
+		popUpTxt = new FlxText(0, 0, 550, mensagem); 
+		popUpTxt.setFormat(Main.gFont, 24, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		popUpTxt.borderSize = 2;
 		popUpTxt.screenCenter();
-		popUpTxt.y -= 30; // Ajusta a altura para não ficar em cima dos botões
+		popUpTxt.y -= 30; 
 		add(popUpTxt);
 
-		// 3. Botões de Escolha
-		// Posicionados em relação à imagem de aviso
-		btnSim = new FlxText(avisoImg.x + 80, popUpTxt.y + 140, 0, "SIM");
-		btnSim.setFormat(Main.gFont, 32, FlxColor.WHITE, CENTER);
+		// 3. Botões
+		btnSim = new FlxText(0, popUpTxt.y + 150, 0, "SIM");
+		btnSim.setFormat(Main.gFont, 36, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		btnSim.x = FlxG.width / 2 - 150;
 		add(btnSim);
 
-		btnNao = new FlxText(avisoImg.x + avisoImg.width - 160, popUpTxt.y + 140, 0, "NÃO");
-		btnNao.setFormat(Main.gFont, 32, FlxColor.WHITE, CENTER);
+		btnNao = new FlxText(0, popUpTxt.y + 150, 0, "NÃO");
+		btnNao.setFormat(Main.gFont, 36, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		btnNao.x = FlxG.width / 2 + 80;
 		add(btnNao);
 
 		atualizarVisualBotoes();
@@ -55,33 +66,37 @@ class WarningState extends MusicBeatState
 	{
 		super.update(elapsed);
 		
-		// Troca a seleção com as setas esquerda/direita
 		if (Controls.justPressed(UI_LEFT) || Controls.justPressed(UI_RIGHT)) {
 			selecionouSim = !selecionouSim;
-			FlxG.sound.play(Paths.sound('scrollMenu')); // Som opcional de menu
+			FlxG.sound.play(Paths.sound('menu/scrollMenu')); 
 			atualizarVisualBotoes();
 		}
 
 		if(Controls.justPressed(ACCEPT))
 		{
-			// Se o jogador escolher SIM, desabilitamos o Flashing (luzes)
-			// Se escolher NÃO, deixamos ativo.
 			FlxG.save.data.flashing = !selecionouSim; 
-			
-           	Init.flagState(); // Continua para o próximo estado do jogo
             FlxG.save.data.beenWarned = true;
             FlxG.save.flush();
+            
+            FlxG.sound.play(Paths.sound('menu/confirmMenu'));
+            
+            // Vai para o Init
+            Init.flagState(); 
         }
 	}
 
 	function atualizarVisualBotoes() {
-		// Destaca o botão selecionado mudando a cor
 		if (selecionouSim) {
 			btnSim.color = FlxColor.YELLOW;
 			btnNao.color = FlxColor.WHITE;
+			btnSim.scale.set(1.2, 1.2);
+			btnNao.scale.set(1, 1);
 		} else {
 			btnSim.color = FlxColor.WHITE;
 			btnNao.color = FlxColor.YELLOW;
+			btnSim.scale.set(1, 1);
+			btnNao.scale.set(1.2, 1.2);
 		}
 	}
 }
+
